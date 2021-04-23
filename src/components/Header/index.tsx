7c446907478b7fd91907f0768022c8106c3e0102
Leandro Sena <leo.sena777@gmail.com>
2021-04-23 20:22:46 -0300

    adds header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logo from "../../assets/logo.svg";
+import { Container, Content } from "./style";
+
+export function Header() {
+  return (
+    <Container>
+      <Content>
+        <img src={logo} alt="MCC logo" />
+        <button type="button">Nova transação</button>
+      </Content>
+    </Container>
+  );
+}
